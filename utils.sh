cc9a93899e3e03093bc03b4b518f5fd32e4977b1
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2023-03-28 11:29:45 -0700

    add utils.sh

---
+++ utils.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+
+function decode_jwt_payload() {
+  local jwt="$1"
+  local payload=$(echo "$jwt" | awk -F '.' '{print $2}')
+  local decoded_payload=$(echo "$payload" | base64 -d)
+  echo "$decoded_payload"
+}
+
+[ -z "$2" ]
+then
+  echo "Error: need second argument"
+  exit 1
+fi
+
+[ "$1" = "urldecode" ]
+then
+  echo $(urldecode $(echo "$2"))
+  exit 0
+fi
+
+[ "$1" = "getjwtpayload" ]
+then
+  echo $(decode_jwt_payload $(echo "$2"))
+  exit 0
+fi
+
+echo "Error: incorrect first argument. Pass either 'urldecode' or 'getjwtpayload'"
+exit 1
